88f4cf47472fdb6cc25f275fef89eb7cb3c64db7
Joshua Johnston <2021johnston.joshua@gmail.com>
2022-07-16 13:49:47 -0400

    idnore .tsx extension

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App.js';
+// @ts-ignore
+import App from './App.tsx';
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root')!);
